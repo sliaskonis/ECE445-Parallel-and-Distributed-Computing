dfc7e35e246dd7eb74f29a014ccde607a4f7b2d2
ostralis <aggelovlr@gmail.com>
2025-01-17 21:34:21 +0200

    works only for numbers that are 2^n

--- hw3/src/ask2.c
+++ hw3/src/ask2.c
@@ -6,38 +6,29 @@
 #include<unistd.h>
 int Broadcast(int id, int number, int max_id) {
     double size = log2((double)(max_id + 1));
-    int mask = 1;
-    int flag = 0;
-    if (id == 0) {flag=1;}
+    if(id==0) MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    if(id==1) PMPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-    for(int i=0; i<=size; i++){
-        mask = mask^(1<<i);
-        if (id==0){
-            printf("2^i=%d\n", (1<<i));
-            printf("MASK=%d\n", mask);
+    for(int i=1; i<size; i++){
+        int partition = (float)(1<<i);
+        // if (id==0){
+        //     printf("partition=%d\n", partition);
+        // }
+        
+        if ((id < partition) && (id <= max_id)){
+            MPI_Send(&number, 1, MPI_INT, (id+partition), 0, MPI_COMM_WORLD);
+            //printf("SEND num%d ::: id=%d, to=%d [%d]\n",number, id, (id+partition), i);
+        }else if(id < 2*partition){
+            //printf("wait id%d from%d\n",id,id-partition);
+            MPI_Recv(&number, 1, MPI_INT, (id-partition), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            //printf("RECV num%d ::: id=%d, from=%d\n",number, id, (id-partition));
         }
-        if (((id & mask) == 0) && ((id ^ (1 << i)) <= max_id)) {
-            if ((id & (1 << i))==0) {
-                // if (flag==1){
-
-                // }
-                MPI_Send(&number, 1, MPI_INT, (id ^ (1 << i)), 0, MPI_COMM_WORLD);
-                //printf("Node %d sent number %d\n", id, number);
-                printf("SEND num%d ::: id=%d, to=%d\n",number, id, (id ^ (1 << i)));
-            }
-            else {
-                //printf("wait for recv id=%d, from=%d |_|_| (i=%d, MASK=%d)\n", id, (id ^ (1 << i)), i, mask);
-                MPI_Recv(&number, 1, MPI_INT, (id ^ (1 << i)), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                printf("RECV num%d ::: id=%d, from=%d\n",number, id, (id ^ (1 << i)));
-                //flag=1;
-                //printf("Node %d received number %d\n", id, number);
-            }
-        }
-        sleep(1);
-        printf("%d\n", id);
-        sleep(1);
+        
+        // sleep(1);
+        // printf("%d\n", id);
+        // sleep(1);
     }
-    MPI_Barrier(MPI_COMM_WORLD);
+    //MPI_Barrier(MPI_COMM_WORLD);
     return number;
 }
 
@@ -81,7 +72,7 @@ int main(int argc, char **argv) {
     }
     MPI_Barrier(MPI_COMM_WORLD);
 
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 1000; i++) {
         MPI_Barrier(MPI_COMM_WORLD); // synchronization point
         start_time = MPI_Wtime(); 
 
