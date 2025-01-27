5ca21c2709a8d44109ffb6ad9b57330c60e5a39e
sliaskonis <spirosliaskonis@gmail.com>
2025-01-27 23:07:30 +0200

    Fixed bug with wrong data being sent (sending a pointer to pointer instead of the real value)

--- hw3/src/ask1.c
+++ hw3/src/ask1.c
@@ -2,32 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "../include/colours.h"
 
-#define NUM_ITER 1000
+#define NUM_ITER 10000
 
-void measure_time(int rank, int tag, void* val, MPI_Datatype datatype) {
-    double start_time, end_time, total_time = 0.0;
-
-    for (int i = 0; i < NUM_ITER; i++) {
-        if (rank == 0) {
-            start_time = MPI_Wtime();
-            MPI_Send(&val, 1, datatype, 1, tag, MPI_COMM_WORLD);
-            MPI_Recv(&val, 1, datatype, 1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            end_time = MPI_Wtime();
-            total_time += (end_time - start_time);
-        } else if (rank == 1) {
-            MPI_Recv(&val, 1, datatype, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(&val, 1, datatype, 0, tag, MPI_COMM_WORLD);
-        }
-    }
-    // στ
-    if (rank == 0) {
-        char* datatype_str = datatype == MPI_INT ? "int" : datatype == MPI_FLOAT ? "float" : "double";
-        printf("Average time for (datatype: %s) = %f seconds\n",
-               datatype_str,
-               total_time / NUM_ITER);
-    }
-}
+void measure_time(int rank, int tag, void *val, MPI_Datatype datatype, const char *datatype_str);
 
 int main(int argc, char **argv) {
     int rank, size;
@@ -36,29 +15,54 @@ int main(int argc, char **argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // a
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
 
-    //b
     if (rank == 0) {
-        printf("Processor: %s, Number of tasks = %d\n", processor_name, size);
-        printf("Hello. This is the master node.\n");
+        printf(GRN"Processor: %s, Number of tasks = %d\n"RESET, processor_name, size);
+        printf(RED "Hello. This is the master node.\n" RESET);
     } else {
-        printf("Hello. This is node %d.\n", rank);
+        printf(BLU "Hello. This is node %d.\n" RESET, rank);
     }
 
-    // c, d, e
+    /*
+     * Measure time for sending and receiving integers, floats and doubles
+     */
     if (rank < 2) {
-        int ivalue = 1;
-        measure_time(rank, 0, &ivalue, MPI_INT);
-        float fvalue = 1.0;
-        measure_time(rank, 1, &fvalue, MPI_FLOAT);
-        double dvalue = 1.0;
-        measure_time(rank, 2, &dvalue, MPI_DOUBLE);
+        int ivalue = rand();
+        measure_time(rank, 0, &ivalue, MPI_INT, "MPI_INT");
+        float fvalue = rand() / (float)RAND_MAX;
+        measure_time(rank, 1, &fvalue, MPI_FLOAT, "MPI_FLOAT");
+        double dvalue = rand() / (double)RAND_MAX;
+        measure_time(rank, 2, &dvalue, MPI_DOUBLE, "MPI_DOUBLE");
     }
 
     MPI_Finalize();
     return 0;
+}
+
+void measure_time(int rank, int tag, void *val, MPI_Datatype datatype, const char *datatype_str) {
+    double start_time, end_time, total_time = 0.0;
+    int type_size;
+    MPI_Type_size(datatype, &type_size);
+    void *recv_val = malloc(type_size);
+
+    for (int i = 0; i < NUM_ITER; i++) {
+        if (rank == 0) {
+            start_time = MPI_Wtime();
+            MPI_Send(val, 1, datatype, 1, tag, MPI_COMM_WORLD);
+            MPI_Recv(val, 1, datatype, 1, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            end_time = MPI_Wtime();
+            total_time += (end_time - start_time) * 1000;
+        } else if (rank == 1) {
+            MPI_Recv(recv_val, 1, datatype, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(recv_val, 1, datatype, 0, tag, MPI_COMM_WORLD);
+        }
+    }
+
+    if (rank == 0) {
+        printf(GRN "Average time for (datatype: %s) = %f ms\n" RESET, datatype_str, total_time / NUM_ITER);
+    }
+    free(recv_val);
 }
